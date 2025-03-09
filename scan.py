93e073046e502b608736640bdcd8a0f29fcf4f4f
robertlay080 <129156551+robertlay080@users.noreply.github.com>
2025-03-09 08:28:26 +0300

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,16 @@
+
+#### **scan.py**
+```python
+import socket
+
+host = "scanme.nmap.org"
+ports = [22, 80, 443, 3306]
+
+for port in ports:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(1)
+        result = s.connect_ex((host, port))
+        if result == 0:
+            print(f"Port {port} is open")
+        else:
+            print(f"Port {port} is closed")
